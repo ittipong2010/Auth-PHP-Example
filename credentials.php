21ec8f9d125f41f80ec20ff2cc23b96a804eda46
ittipong <83745889+ittipong2010@users.noreply.github.com>
2022-07-18 16:49:19 +0700

    Update credentials.php

--- credentials.php
+++ credentials.php
@@ -1,7 +1,7 @@
 <?php
 
-$name = "";         // Application Name
-$ownerid = "";      // Application OwnerID
+$name = "A3";         // Application Name
+$ownerid = "372X914P5W";      // Application OwnerID
 $version = "1.0";   // Application Version
 
 ?>
